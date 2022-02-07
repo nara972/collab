5d2c3e3b76fec384956df9a0516015d38b107382
nara972 <68370958+nara972@users.noreply.github.com>
2022-02-08 00:33:10 +0900

    Update PrincipalOauth2UserService.java

--- collaboration-tool/src/main/java/com/nara/collaboration/config/oauth/PrincipalOauth2UserService.java
+++ collaboration-tool/src/main/java/com/nara/collaboration/config/oauth/PrincipalOauth2UserService.java
@@ -34,6 +34,19 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         //userRequest 정보->loadUser함수 호출->구글로부터 회원프로필 받아줌
         System.out.println("getAttributes:"+oAuth2User.getAttributes());
 
+        String email=oAuth2User.getAttribute("email");
+
+        Optional<User> userEntity=userRepository.findByEmail(email);
+
+        User user=userEntity.orElse(registerNewUser(userRequest));
+
+        return new PrincipalDetails(user,oAuth2User.getAttributes());
+    }
+
+    private User registerNewUser(OAuth2UserRequest userRequest){
+
+        OAuth2User oAuth2User=super.loadUser(userRequest);
+
         //회원가입을 강제로 진행
         String provider=userRequest.getClientRegistration().getClientId(); //google
         String providerId=oAuth2User.getAttribute("sub");
@@ -43,21 +56,17 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         String email=oAuth2User.getAttribute("email");
         String role="ROLE_USER";
 
-        User userEntity=userRepository.findByEmail(email).get();
+        User user=User.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .role(role)
+                .provider(provider)
+                .providerId(providerId)
+                .build();
 
-        if(userEntity==null) {
-            userEntity = User.builder()
-                    .username(username)
-                    .password(password)
-                    .email(email)
-                    .role(role)
-                    .provider(provider)
-                    .providerId(providerId)
-                    .build();
-            userRepository.save(userEntity);
-        }
+        return userRepository.save(user);
 
-        return new PrincipalDetails(userEntity,oAuth2User.getAttributes());
     }
 
 }
